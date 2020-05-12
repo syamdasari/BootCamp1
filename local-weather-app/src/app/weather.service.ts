7f6c03ec04f430869c25253765d80cc8863d779c
Syam Kiran Dasari <Skirand@microsoft.com>
2020-05-11 17:36:13 -0700

    Updated

---
+++ local-weather-app/src/app/weather.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from './../environments/environment';
+import { ICurrentWeather } from './icurrent-weather';
+import { pipe } from '';
+
+
+interface ICurrentWeatherData {
+  weather: [
+    {
+      description: string,
+      icon: string
+    }
+  ];
+  main: {
+    temp: number,
+  };
+  dt: number;
+  sys: {
+    country: string,
+  };
+  name: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  constructor(private httpClient: HttpClient) {
+
+  }
+  getCurrentWeather(city: string, country: string){
+    return this.httpClient.get<ICurrentWeatherData>(
+      `${environment.baseUrl}api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${environment.appId}`.pipe(
+        map(data => this.transformToICurrentWeather(data)))
+   }
+
+  transformToICurrentWeather(data: ICurrentWeatherData) : ICurrentWeather {
+    return {
+      city: data.name,
+      country: data.sys.country,
+      date: new Date(data.dt * 1000),
+      image: 'http://openweathermap.org/img/w/${data.weather[0]/icon}.png',
+      temperature: data.main.temp,
+      description: data.weather[0].description
+      };
+  }
+  convertKelvinToFaheith(kelvin: number): number{
+    return kelvin * 9 / 5 - 459.67;
+  }
+}
